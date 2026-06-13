5e57d117c1c289a1b07a89024c626fa69c777a16
IDRISSA Mane <maneidrissa964@gmail.com>
2026-06-13 11:13:37 +0000

    JS : dark mode avec localStorage, navbar dynamique au scroll

---
+++ js/main.js
@@ -0,0 +1,41 @@
+document.addEventListener('DOMContentLoaded', () => {
+
+
+/* GESTION DU DARK MODE PERSISTANT */
+const body = document.body;
+const themeToggle = document.getElementById('theme-toggle');
+/*recuperer le theme sauvegarde */
+const savedTheme = localStorage.getItem('theme');
+if (savedTheme === 'dark') {
+    body.classList.add('dark-theme');
+}
+//* ecouter le clic sur le bouton de bascule *//
+if (themeToggle) {
+    themeToggle.addEventListener('click', () => {
+        body.classList.toggle('dark-theme');
+
+        //* sauvegarder le choix dans le localstorage *//
+        if (body.classList.contains('dark-theme')) {
+            localStorage.setItem('theme', 'dark');
+         } else {
+             localStorage.setItem('theme', 'light');
+         }
+    } );
+}
+//* NAVBAR DYNAMIQUE et boutton AU SCROLL *//
+const navbar = document.querySelector('.navbar');
+
+window.addEventListener('scroll', () => {
+    //* LOGIQUE NAVBAR *//
+if (navbar){ 
+    if(window.scrollY > 50) {
+        //* si on descend de plus de 50px, on ajoute une classe de scroll *//
+        navbar.classList.add('navbar-scrolled');
+    } else {
+        // * Sinon, on remet la navbar a son etat initial *//
+        navbar.classList.remove('navbar-scrolled');
+    }
+ }
+});
+
+});
